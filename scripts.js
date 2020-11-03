820b0ca8cf899302285a0a310a3c7f016b486e39
Jensen Baker <bakerjensen421@gmail.com>
2020-11-02 23:40:27 -0500

    First Commit

---
+++ scripts.js
@@ -0,0 +1,104 @@
+//Very Easy Challenge
+
+let redApples = 8;
+let greenApples = 3;
+
+let difference = redApples - greenApples;
+
+console.log(`The difference between ${redApples} red apples and ${greenApples} green apples is ${difference} apples!`);
+
+
+
+//Easy Challenge
+
+let myName = 'Jensen'
+let hisName = 'Cudi'
+
+console.log(`My name ${myName}, is 2 characters longer than ${hisName}.`)
+
+
+
+
+//Medium Challenge
+
+/*function capCaller(name) {
+
+    return name == name.toLowerCase() && name != name.toUpperCase();
+}
+
+const name = prompt('Please enter your name here. whisper or SHOUT!');
+if (name === name.toUpperCase()) {
+    alert('You are SHOUTING!')
+}
+if (name === name.toLowerCase()) {
+    alert("I can't hear you!")
+}
+else {
+    alert('We can hear you perfect!')
+}
+
+
+
+console.log(name);
+*/
+
+
+
+// Hard Challenge
+
+function sum(x, y) {
+    return sum = (x + y)
+}
+
+console.log(sum(1, 3))
+
+function subtract(x, y) {
+    return subtract = (x - y)
+}
+
+console.log(subtract(12, 8))
+
+function multiply(x, y) {
+    return multiply = (x * y)
+}
+
+console.log(multiply(3, 3))
+
+function divide(x, y) {
+    return divide = (x / y)
+}
+
+console.log(divide(6, 3))
+
+//Very Hard
+
+const setValues = a, b, results;
+function setValues() {
+    a = Number(document.getElementById("a").value);
+    b = Number(document.getElementById("b").value);
+}
+function sum() {
+    setValues();
+    result = a + b
+    alert("The sum is equal to " + result)
+}
+function subtract() {
+    setValues();
+    result = a - b
+    alert("The rest is equal to " + result)
+}
+function multiply() {
+    setValues();
+    result = a * b
+    alert("The equation is equal to " + result)
+}
+function divide() {
+    setValues();
+    result = a / b
+    alert("The equation is equal to " + result)
+}
+
+
+
+
+
